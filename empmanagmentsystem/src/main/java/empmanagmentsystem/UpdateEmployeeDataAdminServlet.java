7f6f5c5545848ac28f56557ea60a665189549ed8
khelan sahu <110210754+khelansahu@users.noreply.github.com>
2025-03-17 19:46:44 +0530

    UpdateEmployeeDataAdminServlet.java

--- empmanagmentsystem/src/main/java/empmanagmentsystem/UpdateEmployeeDataAdminServlet.java
+++ empmanagmentsystem/src/main/java/empmanagmentsystem/UpdateEmployeeDataAdminServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -17,16 +18,42 @@ public class UpdateEmployeeDataAdminServlet extends HttpServlet{
 
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int id=Integer.parseInt(req.getParameter("id"));
+		String Strid=req.getParameter("id");
 		String name=req.getParameter("nm");
 		String email=req.getParameter("em");
 		String pass=req.getParameter("pwd");
-		int age=Integer.parseInt(req.getParameter("age"));
+		String Strage=req.getParameter("age");
 		String address=req.getParameter("add");
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/practice?user=root&&password=root");
+			PreparedStatement pss=con.prepareStatement("select * from emp where email=?");
+			pss.setString(1, email);
+			ResultSet rs=pss.executeQuery();
+			if (rs.next()) {
+				if (Strid==null || Strid.isEmpty()) {
+					Strid = Integer.toString(rs.getInt("id"));
+				}
+				if (name==null  || name.isEmpty() ) {
+					name = rs.getString("name");
+				}
+				if (email==null || email.isEmpty()) {
+					email = rs.getString("email");
+				}
+				if (pass==null || pass.isEmpty()) {
+					pass = rs.getString("password");
+				}
+				if (Strage==null || Strage.isEmpty()) {
+					Strage = Integer.toString(rs.getInt("age"));
+				}
+				if (address==null || address.isEmpty()) {
+					address = rs.getString("address");
+				}
+			}
+			int id= Integer.parseInt(Strid);
+			int age=Integer.parseInt(Strage);
+				
 			PreparedStatement ps=con.prepareStatement("update emp set id=?, name=?, password=?, age=?, address=? where email=?");
 			ps.setInt(1, id);
 			ps.setString(2, name);
